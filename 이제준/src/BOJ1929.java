34256bcbb94a8dca1bb8a09287c0c119ee5edcf3
LeeJejune <leejejune0625@gmail.com>
2022-05-14 11:53:30 +0900

    [BOJ] 소수 구하기 / 실버 3 / 20분 "https://www.acmicpc.net/problem/1929"

---
+++ 이제준/src/BOJ1929.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+public class BOJ1929 {
+    public static boolean[] prime;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringBuilder sb = new StringBuilder();
+
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        prime = new boolean[n + 1];
+        get_prime();
+
+
+        for(int i = m; i <= n; i++) {
+            if(!prime[i]) {
+                sb.append(i).append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+
+    public static void get_prime() {
+        prime[0] = prime[1] = true;
+
+        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if(prime[i]) continue;
+            for(int j = i * i; j < prime.length; j+=i) {
+                prime[j] = true;
+            }
+        }
+    }
+}
