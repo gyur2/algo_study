1495ebb3a87ac6090acb70a3f0eeb89ad0fb0917
LeeJejune <leejejune0625@gmail.com>
2022-05-27 17:21:29 +0900

    [BOJ] 반복문/ 브론즈1~3 / 총 40분 "https://www.acmicpc.net/step/3"

---
+++ 이제준/src/BOJ10952.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class BOJ10952 {
+    public static void main(String[] args) throws IOException {
+        //백준 10952
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st;
+//
+//        while (true){
+//            st = new StringTokenizer(br.readLine()," ");
+//            int n = Integer.parseInt(st.nextToken());
+//            int m = Integer.parseInt(st.nextToken());
+//
+//            if (n == 0 && m ==0){
+//                break;
+//            }
+//
+//            System.out.println((n+m));
+//        }
+//      백준 10951
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNextInt()){
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+
+        }
+        sc.close();
+
+    }
+}
---
+++ 이제준/src/BOJ2438.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+//public class BOJ2438 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        for (int i= 1; i <= n; i++){
+//            for (int j = 1; j <= i; j++){
+//                System.out.print('*');
+//            }
+//            System.out.println(" ");
+//        }
+//
+//    }
+//}
+
+public class BOJ2438 {
+    //별찍기 2
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i= 1; i <= n; i++){
+            for (int j = 1; j <= n - i; j++){
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
