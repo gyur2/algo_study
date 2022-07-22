2a7eac5f64e7e35977c894296ce18b7f09f4d767
LeeJejune <leejejune0625@gmail.com>
2022-07-22 20:43:44 +0900

    [BOJ] 재귀/ 실버5~골드5 / 60분 "https://www.acmicpc.net/step/19"

---
+++ 이제준/src/recursion/BOJ10872.java
@@ -0,0 +1,16 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class BOJ10872 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 1;
+
+        for (int i = 1; i <= n; i++) {
+            sum *= i;
+        }
+        System.out.println(sum);
+    }
+}
