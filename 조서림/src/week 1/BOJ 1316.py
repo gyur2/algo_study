a6836de4bb65206d6ee4dd19b8173654fd20e7a3
srcho01 <srcho01@naver.com>
2023-01-14 01:18:59 +0900

    [BOJ] 그룹 단어 체커 / 실버 V / 20분 https://www.acmicpc.net/problem/1316

---
+++ 조서림/src/week 1/BOJ 1316.py
@@ -0,0 +1,25 @@
+N = int(input())
+
+words = []
+for _ in range(N):
+    words.append(input())
+    
+answer = N
+for word in words:
+    check = [0] * 26
+    prev = word[0]
+    check[ord(prev) - 97] = 1 # a는 97
+    
+    for i in range(1, len(word)):
+        curr = word[i]
+        if (check[ord(curr) - 97] == 0) or (check[ord(curr) - 97] == 1 and prev == curr):
+            prev = curr
+            check[ord(prev) - 97] = 1
+        else:
+            answer -= 1
+            break
+
+print(answer)
+
+# check == 0 : 상관 없음
+# chekc == 1 : prev와 curr가 같으면 괜찮음, prev와 curr가 다르면 정답에서 제외
