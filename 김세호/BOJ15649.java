e28b9e08223797d30c2e14b76c2aff5bdd22fcfb
tpgh02 <tpgh02@kookmin.ac.kr>
2023-02-02 18:09:32 +0900

    [BOJ] N과 M (1) / 실버3 / x

---
+++ 김세호/BOJ15649.java
@@ -0,0 +1,25 @@
+// 12:50
+
+package 김세호;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ15649 {
+    void ans(int arr[]){
+        
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        
+
+    }
+}
