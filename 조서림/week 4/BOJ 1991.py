c856ebec591629e8220313f2651a752314c131e9
srcho01 <srcho01@naver.com>
2023-02-15 03:18:56 +0900

    [BOJ] 트리 순회 / 실버 I / 2시간 https://www.acmicpc.net/problem/1991

---
+++ 조서림/week 4/BOJ 1991.py
@@ -0,0 +1,36 @@
+n = int(input())
+
+def preorder(root):
+    if root != '.':
+        print(root, end='')
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])
+        print(root, end='')
+        inorder(tree[root][1])
+        
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        print(root, end='')
+
+
+tree = {}
+
+for _ in range(n):
+    key, left, right = map(str, input().split())
+    tree[key] = [left,right]
+
+
+preorder("A")
+print()
+
+inorder("A")
+print()
+
+postorder("A")
+print()
